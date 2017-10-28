74f584d2aee47d085d7be2b10192fa5313de77a6
martins <saulomartins.martins@gmail.com>
2017-10-28 05:12:49 -0200

    Segundo commit dos scripts

--- t_necessidades_especiais/t_necessidades_especiais_2015.py
+++ t_necessidades_especiais/t_necessidades_especiais_2015.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-arquivoentrada = open('microdados_enem_2014.csv', 'r')
-arquivosaida = open('t_necessidadesespeciais_1.csv', 'w')
+arquivoentrada = open('microdados_enem_2015.csv', 'r')
+
 
 for linha in arquivoentrada:
     texto = linha.split(",")
 
     arquivosaida.writelines(
-        texto[0] + "," + texto[51] + "," + texto[31] + "," + texto[34] + "," + texto[33] + "," + texto[49] + "," + \
-        texto[50] + "," + texto[53] + "," + texto[35] + "," + texto[28] + "," + texto[52] + "," + texto[47] + "," + texto[41] + "," + texto[36] + "," +
-        texto[37] + ","
-        + texto[29] + "," + texto[48] + "," + texto[38] + "," + texto[30] + "," + texto[40] + "," + texto[39] + "," +
-        texto[45] + "," + texto[46] + '\n')
+        texto[0] + "," + texto[57] + "," + texto[32] + "," + texto[35] + "," + texto[34] + "," + texto[55] + "," + \
+        texto[56] + "," + texto[59] + "," + texto[36] + "," + texto[29] + "," + texto[58] + "," + texto[53] + "," + \
+        texto[44] + "," + texto[37] + "," + texto[38] + ","  + texto[30] + "," + texto[54] + "," + texto[39] + "," + \
+        texto[31] + "," + texto[42] + "," + texto[40] + "," +texto[51] + "," + texto[52] + '\n')
+
+
+
 
 arquivoentrada.close()
-arquivosaida.close()
