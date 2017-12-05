54e6e57ad410757a3bf4518a84ae5a5ab0302a6c
martins <saulomartins.martins@gmail.com>
2017-12-05 03:56:24 -0200

    Commit of function shell

--- t_candidato/t_func_split.sh
+++ t_candidato/t_func_split.sh
@@ -4,8 +4,11 @@ function split_arquivo() {
 
 	nome_arquivo=$1
 
-    count=`wc --lines $1`
+    count=`wc -l $1 | awk '{print $1}'`
 
-    string="Quantidade de linhas: "
-    echo $string$count
+    count1=21
+    string="Soma: "
+
+
+    echo $(($count+$count1))
 }
