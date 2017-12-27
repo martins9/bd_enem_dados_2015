3c2846bad77b337c6a100b249c51ecada1b106c4
martins <saulomartins.martins@gmail.com>
2017-12-27 03:43:46 -0200

    Novos Commits

--- t_candidato/t_candidato_main.sh
+++ t_candidato/t_candidato_main.sh
@@ -13,8 +13,8 @@ cd ..
 
 # Chamando a funcao split
 source ./t_funcoes_shell/t_func_split.sh && arq_original=$path1/microdados_enem_2015_1.csv && diretorio=t_candidato_2015 \
-&& arq_final=t_candidato_2015_ && split_arquivo $arq_original $path1 $pasta $arq_final
+&& arq_final=t_candidato_2015_ && split_arquivo $path1 $diretorio $arq_original $pasta $arq_final
 
 # Fazendo o load para a tabela
-sorce ./t_funcoes_shell/t_func_load.sh && diretorio=t_candidato_2015 && nome_tabela=t_candidato && arq_uniq
-load_tabela $diretorio $nome_tabela $arq_uniq
+sorce ./t_funcoes_shell/t_func_load.sh && diretorio=t_candidato_2015 && nome_tabela=t_candidato && arquivo_original= &&
+load_tabela $path1 $diretorio $arquivo_original $pasta $nome_tabela
--- t_funcoes_shell/t_func_load.sh
+++ t_funcoes_shell/t_func_load.sh
@@ -6,28 +6,33 @@ function load_tabela() {
 	path1=$2
     pasta=$3
     nome_tabela=$4
+    arquivo_original=$5
 
-if [ -n $2 ] && [ -n $3 ] && [ -n $4 ]
+if [ -n $2 ] && [ -n $3 ] && [ -n $4 ] && [ -z $5 ]; then
+
 
     # Contando quantos arquivos tem na pasta
     QTD_ARQ=$(find $2/$3/ -type f | wc -l)
 
     # Pegando data e hora
     data=$(date "+%d/%m/%Y %H:%M:%S")
-    echo "------------- START:" $data "-------------" > $2_log.txt
+    echo "------------- START:" $data "-------------" > $4_log.txt
 
     QTD_ARQ1=$((QTD_ARQ-1))
     # Fazendo o Load das tabelas
     for i in $(seq -f "%06g" 0 $QTD_ARQ1)
     do
         # psql -h localhost -d bd_enem_dados_local -U anacrl -c
-	    time (psql -h localhost -d bd_enem_dados -U anacrl -c "\copy $4 from $path1/$3/t_responde_2014_$i.csv
-	        with delimiter as ',' NULL AS 'null' csv") &>> $1_log.txt
+	    time (psql -h localhost -d bd_enem_dados -U anacrl -c "\copy $4 from $2/$3/t_responde_2014_$i.csv
+	        with delimiter as ',' NULL AS 'null' csv") &>> $4_log.txt
     done
 
     # Pegando data e hora
     data1=$(date "+%d/%m/%Y %H:%M:%S")
     echo "------------- START:" $data "-------------" >> $1_log.txt
     echo
+elif [ -n $2 ] && [ -z $3 ] && [ -n $4 ] && [ -z $5 ]
+
+
 
 }
