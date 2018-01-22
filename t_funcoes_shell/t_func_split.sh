609a11d189eb20a7cf1b9cfad362e281a588ed7b
martins <saulomartins.martins@gmail.com>
2018-01-22 19:34:51 -0200

    Novo Commit

--- t_funcoes_shell/t_func_split.sh
+++ t_funcoes_shell/t_func_split.sh
@@ -2,21 +2,15 @@
 
 function split_arquivo() {
 
-    # Parametros para a funcao
-	nome_arquivo=$1
-	path1=$2
-    pasta=$3
-    arq_final=$4
-
     # Contando quantas linhas tem no arquivo
-    count=`wc -l $nome_arquivo | awk '{print $1}'`
+    count=$(wc -l $arquivo_original | awk '{print $1}')
 
     # Regra para fazer o split do arquivo
     if [ "$count" -lt 10000000 ]; then
-    	split -d -a6 -l 1000000 --additional-suffix=.csv $1 $2/$3/$4
+    	split -d -a6 -l 1000000 --additional-suffix=.csv $arquivo_original $path1/$pasta/$arq_final
     elif [ "$count" -ge 10000001 ] && [ "$count" -lt 80000000 ]; then
-    	split -d -a6 -l 3000000 --additional-suffix=.csv $1 $2/$3/$4
+    	split -d -a6 -l 3000000 --additional-suffix=.csv $arquivo_original $path1/$pasta/$arq_final
     elif [ "$count" -ge 80000001 ]; then
-        split -d -a6 -l 5000000 --additional-suffix=.csv $1 $2/$3/$4
+        split -d -a6 -l 5000000 --additional-suffix=.csv $arquivo_original $path1/$pasta/$arq_final
     fi
 }
