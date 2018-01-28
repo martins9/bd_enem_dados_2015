ca95772d3a6c6987b199dbbdb648f45d47ee3870
martins <saulomartins.martins@gmail.com>
2018-01-28 02:43:47 -0200

    Novo Commit

--- t_area/t_area_2015.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO AREA(cod_area, nome_area, sigla_area) VALUES (1, 'PROVA DE CIENCIA DA NATUREZA', 'CN');
-INSERT INTO AREA(cod_area, nome_area, sigla_area) VALUES (2, 'PROVA DE CIENCIA HUMANAS', 'CH');
-INSERT INTO AREA(cod_area, nome_area, sigla_area) VALUES (3, 'PROVA DE LINGUAGENS E CODIGOS', 'LC');
-INSERT INTO AREA(cod_area, nome_area, sigla_area) VALUES (4, 'PROVA DE MATEMATICA', 'MT');
