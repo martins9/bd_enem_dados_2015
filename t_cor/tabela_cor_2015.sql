fa18111fa61002ba328ff3ca9ee0c91aac6fc538
martins <saulomartins.martins@gmail.com>
2018-01-28 02:45:50 -0200

    Novo Commit

---
+++ t_cor/tabela_cor_2015.sql
@@ -0,0 +1,12 @@
+INSERT INTO COR (cod_cor, nome_cor) VALUES (1, 'AZUL');
+INSERT INTO COR (cod_cor, nome_cor) VALUES (2, 'AMARELO');
+INSERT INTO COR (cod_cor, nome_cor) VALUES (3, 'BRANCO');
+INSERT INTO COR (cod_cor, nome_cor) VALUES (4, 'ROSA');
+INSERT INTO COR (cod_cor, nome_cor) VALUES (5, 'BRANCO - ADAPTADA (BRAILE E LEDOR)');
+INSERT INTO COR (cod_cor, nome_cor) VALUES (6, 'AZUL (REAPLICAÇÃO)');
+INSERT INTO COR (cod_cor, nome_cor) VALUES (7, 'AMARELO (REAPLICAÇÃO)');
+INSERT INTO COR (cod_cor, nome_cor) VALUES (8, 'BRANCO (REAPLICAÇÃO)');
+INSERT INTO COR (cod_cor, nome_cor) VALUES (9, 'ROSA (REAPLICAÇÃO)');
+INSERT INTO COR (cod_cor, nome_cor) VALUES (10, 'CINZA');
+INSERT INTO COR (cod_cor, nome_cor) VALUES (11, 'CINZA - ADAPTADA (BRAILE E LEDOR)');
+INSERT INTO COR (cod_cor, nome_cor) VALUES (12, 'CINZA (REAPLICAÇÃO)');
