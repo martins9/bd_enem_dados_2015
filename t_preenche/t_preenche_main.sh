50fd3d68f8038b970e4b38daeb8bda216cbc1c76
martins <saulomartins.martins@gmail.com>
2018-02-12 03:16:49 -0200

    Novo Commit

--- t_preenche/t_preenche_main.sh
+++ t_preenche/t_preenche_main.sh
@@ -11,3 +11,21 @@ python t_preenche_2015_2.py
 
 # Excluindo os arquivos
 rm t_preenche_2015.csv t_preenche_2015_1.csv
+
+# Fazendo o load para a tabela
+pasta="t_preenche_2015"
+arquivo_original="t_preenche_2015_final.csv"
+arq_final="t_preenche_2015_"
+source /home/anacrl/microdados_enem_2015/t_funcoes_shell/t_func_split.sh
+split_arquivo $path1 $pasta $arquivo_original $arq_final
+
+# Removendo arquivo
+rm t_preenche_2015_final.csv
+
+# Fazendo o load para a tabela
+diretorio="t_preenche_2015"
+nome_tabela="preenche"
+arquivo_original="empty"
+arquivo_final="t_preenche_2015_"
+source /home/anacrl/microdados_enem_2015/t_funcoes_shell/t_func_load.sh
+load_tabela $path1 $diretorio $nome_tabela $arquivo_original $arquivo_final
